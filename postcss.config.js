54b5c3bb303d21411a6fc1e7c6ef2f32baab02a6
xiaolongxi <2890714832@qq.com>
2018-03-20 12:00:47 +0800

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// 优化css代码 兼容浏览器
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+	plugins: [
+		autoprefixer()
+	]
+}
---
+++ src/todo/header.vue
@@ -0,0 +1,21 @@
+<template>
+	<header class="main-header">
+		<h1>{{msg}}</h1>
+	</header>
+</template>
+<script>
+	export default {
+		data () {
+			return {
+				msg: 'Jtodo'
+			}
+		}
+	}
+</script>
+<style>
+	.main-header{
+		font-size: 20px;
+		color: #f0f;
+		text-align: center;
+	}
+</style>
